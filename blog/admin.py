11433d645997f4b2e937d15760e4cb31e73eb905
phj539 <84316878+phj539@users.noreply.github.com>
2021-05-18 17:09:11 +0900

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-<<<<<<< HEAD
-admin.site.register(Post)
-=======
+
 admin.site.register(Post)
 
 # Register your models here.
->>>>>>> 6a018f7998af115da34b08cfb3ca5817130be73d
+
